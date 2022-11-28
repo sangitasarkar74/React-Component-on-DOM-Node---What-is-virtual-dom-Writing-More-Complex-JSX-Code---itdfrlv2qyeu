48b9a4534a9e64eff4eb41c82582ae72462895c3
sangitasarkar74 <sangitasarkar@gmail.com>
2022-11-29 00:10:13 +0530

    Stage one

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,15 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
+import '../styles/App.css';
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <p>
+                Now I can render any React component on any DOM node I want using ReactDOM.render
+            </p>
+        </div>
+    );
+};
+
+export default App;
